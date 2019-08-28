8dd1d297d47c4c400f49f257ae63f8180386b725
Eric Odhiambo <odhiamboeric0049@gmail.com>
2019-08-28 20:54:25 +0300

    updates

--- nearbyshops/settings.py
+++ nearbyshops/settings.py
@@ -144,5 +144,5 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
-GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
+# GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
+# GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
